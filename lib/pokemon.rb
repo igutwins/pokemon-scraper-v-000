b85f736666392cccd7e8e1a7905dd3b993bffdc7
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-09 00:27:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
-attr_accessor :name, :type, :db
+attr_accessor :name, :type, :db, :id
 
-def initialize(name, type, db)
+def initialize(name, type, db, id=nil)
   @name = name
   @type = type
   @db = db
