10e5ac71b2f79c21200c06569185cc1fa3b34747
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-09 01:24:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -41,9 +41,6 @@ def self.find(id, db) #finds a pokemon from the database by their id number and 
       WHERE id = id
   SQL
   row = db.execute(sql)
-  id = row[0]
-  name = row[1]
-  type = row[2]
   new_poke = self.new(id: row[0], name: row[1], type: row[2], db: db)
   new_poke
 end
