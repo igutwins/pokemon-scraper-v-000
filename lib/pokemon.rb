d0f951102b521789b2f61508bc3cf8bb582cb3ff
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-09 00:23:19 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
+attr_accessor :name, :type, :db
+
+def initialize(name, type, db)
+
+end
+
+def self.create
+
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS pokemon (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      type TEXT
+    );
+  SQL
+
+end
+
+def self.save
+  #create
+  #remove
+
 end
